2680fbc90bd4faebccb23b335adf3e6cc3b6d211
fadnihdr <fadnihdr@gmail.com>
2019-11-29 22:23:14 +0700

    revamp

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-var masoCount = 0;
+let masoCount = 0;
 document.getElementById("counter").innerHTML = "Counter = " + masoCount;
 
 
 function multiplier() {
     let count = arguments[0];
-    let tocheck = arguments[1];
+//  let tocheck = arguments[1];
     let tmp = "";
     let string1 = "";
+/**
     if (fadnimaso(tocheck)){
         tmp = "cieee. aku udah fix ini kok :). kamu gabisa macam2 beneran lagi. fadni nggabisa maso. kamu aja yang bisa lalalalalalalala"
     }
+
     else tmp = arguments[1];
+ **/
+    tmp = arguments[1];
     for (i = 0; i < count; i++){
         string1 = string1 + "<br>" + tmp;
     }
@@ -18,6 +22,7 @@ function multiplier() {
 
 }
 
+/**
 function fadnimaso() {
     let str = arguments[0];
     let uniq = "";
@@ -42,34 +47,75 @@ function fadnimaso() {
 
 }
 
+
 function showimage() {
     var img = document.createElement("IMG");
     img.src = "assets/img/unibrow.jpg";
     img.size = "800x450";
     document.getElementById("image").appendChild(img);
 }
+**/
 
-function defthanks1() {
-    let n = 1000;
-    let str = "Terimakasih yola atas semuanya, kamu bikin aku seneng. Aku seneng udah bikin kamu seneng";
+function showimg() {
+    let img = document.createElement("IMG");
+    let num = Math.random() * 10;
+    switch (num) {
+        case 0:
+            img.src = "assets/img/2kitten";
+            break;
+        case 1:
+            img.src = "assets/img/6";
+            break;
+        case 2:
+            img.src = "assets/img/7";
+            break;
+        case 3:
+            img.src = "assets/img/cuddle1";
+            break;
+        case 4:
+            img.src = "assets/img/cuddle2";
+            break;
+        case 5:
+            img.src = "assets/img/cuddle3";
+            break;
+        case 6:
+            img.src = "assets/img/wht1";
+            break;
+        case 7:
+            img.src = "assets/img/wht2";
+            break;
+        case 8:
+            img.src = "assets/img/cat3";
+            break;
+        case 9:
+            img.src = "assets/img/cuddle4";
+
+    }
+    document.getElementById("image").appendChild(img);
+
+}
+
+function iloveyou() {
+    let n = 3000;
+    let str = "I love you 3000 boo. Good work for today. Get rid of all the tiring thoughts. Hope you get home safely <3";
     document.getElementById("output").innerHTML = multiplier(n, str);
 }
 
-function defthanks2() {
-    let n = 5000;
-    let str = "Terimakasih yola atas semuanya, kamu bikin aku seneng. Aku seneng udah bikin kamu seneng";
+function aishiteru() {
+    let n = 3000;
+    let str = "アイステル。 グーグル翻訳を使用しないでください >w<";
     document.getElementById("output").innerHTML = multiplier(n, str)
 }
 
-function defsorry1() {
-    let n = 1000;
-    let str = "Maafin aku ya yola udah bikin masalah ke hidup kamu. Please stay strong, aku dan yang lain2 masih support kamu kok"
+function saranghae() {
+    let n = 3000;
+    let str = "사랑해 여보. 구글 번역을 시도하지 마십시오 :)";
     document.getElementById("output").innerHTML = multiplier(n, str)
 }
 
-function defsorry2() {
-    let n = 5000;
-    let str = "Maafin aku ya yola udah bikin masalah ke hidup kamu. Please stay strong, aku dan yang lain2 masih support kamu kok"
+function sayangkamu() {
+    let n = 3000;
+    let str = "Aku sayang kamu <3. Capek banget ya hari ini UwU. capek2 tapi lebih dari 1000 juga :(. Gapapa, ntar dirumah bisa langsung tepar. Hati2 ya dijalan boo";
     document.getElementById("output").innerHTML = multiplier(n, str)
 }
 
